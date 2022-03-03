dde602320ed2be24c1e3c653b87a968f26b790b8
canveo <canveo@gmail.com>
2022-03-03 12:34:45 -0300

    se adicionan parentesis

--- src/ros_essentials_cpp/src/topic02_motion/turtlebot/turtlebot_move.py
+++ src/ros_essentials_cpp/src/topic02_motion/turtlebot/turtlebot_move.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python3
 import rospy
 from geometry_msgs.msg import Twist
 import math
@@ -9,46 +8,46 @@ from std_srvs.srv import Empty
 
 
 def move(speed, distance, is_forward):
-        #declare a Twist message to send velocity commands
-        velocity_message = Twist()
-        #get current location 
+    #declare a Twist message to send velocity commands
+    velocity_message = Twist()
+    #get current location 
 
 
-        if (speed > 0.4):
-            print 'speed must be lower than 0.4'
-            return
+    if (speed > 0.4):
+        print('speed must be lower than 0.4')
+        return
 
-        if (is_forward):
-            velocity_message.linear.x =abs(speed)
-        else:
-        	velocity_message.linear.x =-abs(speed)
+    if (is_forward):
+        velocity_message.linear.x =abs(speed)
+    else:
+        velocity_message.linear.x =-abs(speed)
 
-        distance_moved = 0.0
-        loop_rate = rospy.Rate(10) # we publish the velocity at 10 Hz (10 times a second)    
-        cmd_vel_topic='/cmd_vel_mux/input/teleop'
-        velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
+    distance_moved = 0.0
+    loop_rate = rospy.Rate(10) # we publish the velocity at 10 Hz (10 times a second)    
+    cmd_vel_topic='/cmd_vel_mux/input/teleop'
+    velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
 
-        t0 = rospy.Time.now().to_sec()
+    t0 = rospy.Time.now().to_sec()
 
-        while True :
-                rospy.loginfo("Turtlesim moves forwards")
-                velocity_publisher.publish(velocity_message)
+    while True :
+            rospy.loginfo("Turtlesim moves forwards")
+            velocity_publisher.publish(velocity_message)
 
-                loop_rate.sleep()
-                t1 =  rospy.Time.now().to_sec()
-                #rospy.Duration(1.0)
-                
-                distance_moved = (t1-t0) * speed
-                print  distance_moved               
-                if  not (distance_moved<distance):
-                    rospy.loginfo("reached")
-                    break
-        
-        #finally, stop the robot when the distance is moved
-        velocity_message.linear.x =0
-        velocity_publisher.publish(velocity_message)
+            loop_rate.sleep()
+            t1 =  rospy.Time.now().to_sec()
+            #rospy.Duration(1.0)
+            
+            distance_moved = (t1-t0) * speed
+            print(distance_moved)            
+            if  not (distance_moved<distance):
+                rospy.loginfo("reached")
+                break
     
-def rotate (angular_speed_degree, relative_angle_degree, clockwise):
+    #finally, stop the robot when the distance is moved
+    velocity_message.linear.x =0
+    velocity_publisher.publish(velocity_message)
+    
+def rotate(angular_speed_degree, relative_angle_degree, clockwise):
     
 
     velocity_message = Twist()
@@ -81,7 +80,7 @@ def rotate (angular_speed_degree, relative_angle_degree, clockwise):
         current_angle_degree = (t1-t0)*angular_speed_degree
         loop_rate.sleep()
 
-        print 'current_angle_degree: ',current_angle_degree
+        print('current_angle_degree: ',current_angle_degree)
                        
         if  (current_angle_degree>relative_angle_degree):
             rospy.loginfo("reached")
@@ -97,7 +96,7 @@ def go_to_goal(x_goal, y_goal):
     global y, z, yaw
 
     velocity_message = Twist()
-    cmd_vel_topic='/turtle1/cmd_vel'
+    cmd_vel_topic = '/turtle1/cmd_vel'
 
     while (True):
         K_linear = 0.5 
@@ -113,7 +112,7 @@ def go_to_goal(x_goal, y_goal):
         velocity_message.angular.z = angular_speed
 
         velocity_publisher.publish(velocity_message)
-        print 'x=', x, 'y=',y
+        print('x=', x, 'y=',y)
 
 
         if (distance <0.01):
@@ -132,3 +131,4 @@ if __name__ == '__main__':
        
     except rospy.ROSInterruptException:
         rospy.loginfo("node terminated.")
+
